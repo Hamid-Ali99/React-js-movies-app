83d6c91e9b7c97d5d894f18be70f4dcda93ce27d
Hamid <hamidali006789@gmail.com>
2024-06-23 17:14:04 +0500

    tmdb api setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { fetchApiData } from "./utils/api.ts";
+
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchApiData("/movie/popular").then((res: []) => {
+      console.log(res);
+    });
+  };
   return (
     <>
       <div>Home</div>
