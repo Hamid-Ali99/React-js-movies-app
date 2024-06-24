448414cbaa93ab1409f3ddddeed421bb44271799
Hamid <hamidali006789@gmail.com>
2024-06-24 09:26:52 +0500

    created initial pages, components and setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from "react";
-import { fetchApiData } from "./utils/api.ts";
-
 import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { fetchApiData } from "./utils/api.ts";
 import { getApiConfig } from "./store/homeSlice.ts";
+import Header from "./components/header/Header.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Home from "./pages/home/Home.tsx";
+import Details from "./pages/details/Details.tsx";
+import SearchResult from "./pages/home/searchResult/SearchResult.tsx";
+import Explore from "./pages/explore/Explore.tsx";
+import PageNotFound from "./pages/404/PageNotFound.tsx";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,12 +30,18 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        Home
-        <p>{url?.total_pages}</p>
-      </div>
-    </>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
