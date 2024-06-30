f09598f9bcfe91c8c887e266b00cc970e8534273
Hamid <hamidali006789@gmail.com>
2024-06-30 09:57:01 +0500

    Created popular and topRated section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,17 @@
 import HeroBanner from "./heroBanner/HeroBanner";
-import "./style.scss";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
+
+import "./style.scss";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
+      <Popular />
+      <TopRated />
     </div>
   );
 };
